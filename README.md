40eedfd53ff6d2cfb4deba199ab9a36e8cbed5fc
Mike Wilcox <mwilcox56@gmail.com>
2014-10-28 22:23:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+misc-utils
+=======
+
+![nothing to see here](/barbrady.jpg)
+
+## License
+[MIT License](https://raw.githubusercontent.com/mjw56/misc-utils/master/LICENSE)
---
+++ nasa-csv-thesaurus-convert/converter.js
@@ -0,0 +1,52 @@
+var fs = require('fs'),
+    readline = require('readline'),
+    thesaurus = {}
+
+readFile(function() {
+  writeFile();
+});
+
+
+function readFile(callback) {
+  fs.readFile('NASA_Thesaurus_Alpha_CSV.txt', { encoding: 'utf8'}, function(err, data) {
+    if(err) {
+      console.log(err);
+    }
+
+    convert(data.split('\n'), function() {
+      callback();
+    });
+  });
+}
+
+function writeFile() {
+  var outputFilename = 'thesaurus.json';
+
+  fs.writeFile(outputFilename, JSON.stringify(thesaurus, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + outputFilename);
+    }
+  });
+}
+
+function convert(data, callback) {
+  for(var i = 0; i < data.length; i++) {
+    var tokens = data[i].split(',');
+
+    var key = _scrub(tokens[1]);
+
+    if(!thesaurus[key]) {
+      thesaurus[key] = _scrub(tokens[5]);
+    }
+  }
+
+  callback();
+}
+
+function _scrub(word) {
+  if(!word) return;
+  word = word.replace(/"/g,'');
+  return(word.toString());
+}
